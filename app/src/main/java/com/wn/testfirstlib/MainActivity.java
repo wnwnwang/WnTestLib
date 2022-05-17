9de6864a26de7240f5da4b1290de9cbd269f474f
wnwnwang <wnwnwang@sina.com>
2022-05-17 16:30:24 +0800

    init code

---
+++ app/src/main/java/com/wn/testfirstlib/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wn.testfirstlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wn.messagelib.MyUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ messageLib/src/main/java/com/wn/messagelib/MyUtil.java
@@ -0,0 +1,21 @@
+package com.wn.messagelib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Copyright (C), 2015/6/12, 日照安泰科技发展有限公司
+ * Author: wangning
+ * Date: 2022/5/17
+ * Description:
+ * <p>
+ * </p>
+ * History:
+ * <author>      <time>      <version>      <desc>
+ * 作者姓名       修改时间     版本号         描述
+ */
+public class MyUtil {
+    public static void showMsg(Context context) {
+        Toast.makeText(context, "xixixixxi", Toast.LENGTH_SHORT).show();
+    }
+}
